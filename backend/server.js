35e105cec07aba41079941a04d2d62b22648f25f
sasank <kotasasank123@gmail.com>
2025-11-15 19:22:06 +0530

    fixed health endpoint

--- backend/server.js
+++ backend/server.js
@@ -1595,9 +1595,9 @@ app.get("/debug-cors", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`🚀 Server is running on http://localhost:${port}`);
+// });
 
 
 
